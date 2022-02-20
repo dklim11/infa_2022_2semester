83cefacbc36698ae8f448cf0733039cc82b2101f
Klimanov <klimanov.dd@phystech.edu>
2022-02-20 12:09:20 +0300

    Programm is working out for first point

--- Lesson_2/Experiment/main.cpp
+++ Lesson_2/Experiment/main.cpp
@@ -11,7 +11,7 @@ int random(int bound) {
 	return dist(rng);
 }
 
-bool check(int (&array)[5][5], int i, int j) {
+bool check(int (&array)[10][10], int i, int j) {
 	bool flag = false;
 	if (array[i - 1][j] == 1 || array[i - 1][j] == 2) {
 		flag = true;
@@ -29,30 +29,35 @@ bool check(int (&array)[5][5], int i, int j) {
 	return flag;
 }
 
-void move(int (&array)[5][5], int size) {
+void move(int (&array)[10][10], int size) {
+	int movement[size][size] = {0};
 	int direction;
 	for (int i = 0; i < size; i++) {
 		for (int j = 0; j < size; j++) {
-			if (array[i][j] == 1) {
-				direction = random(4);
+			if (array[i][j] == 1 && movement[i][j] == 0) {
+				direction = random(3);
 				if (direction == 0) {
 					if (array[i - 1][j] == 0) {
 						array[i - 1][j] = 1;
+						movement[i - 1][j] = 1;
 						array[i][j] = 0;
 					}
 				} else if (direction == 1) {
 					if (array[i][j - 1] == 0) {
 						array[i][j - 1] = 1;
+						movement[i][j - 1] = 1;
 						array[i][j] = 0;
 					}
 				} else if (direction == 2) {
 					if (array[i + 1][j] == 0) {
 						array[i + 1][j] = 1;
+						movement[i + 1][j] = 1;
 						array[i][j] = 0;
 					}
 				} else {
 					if (array[i][j + 1] == 0) {
 						array[i][j + 1] = 1;
+						movement[i][j + 1] = 1;
 						array[i][j] = 0;
 					}
 				}
@@ -61,7 +66,7 @@ void move(int (&array)[5][5], int size) {
 	}
 }
 
-void step(int (&array)[5][5], int size, int &quantity) {
+void step(int (&array)[10][10], int size, int &quantity) {
 	for (int i = 0; i < size; i++) {
 		for (int j = 0; j < size; j++) {
 			if (array[i][j] == 1 && (i == 0 || i == size - 1 || j == 0 || j == size - 1)) {
@@ -79,26 +84,42 @@ void step(int (&array)[5][5], int size, int &quantity) {
 	move(array, size);
 }
 
-int main() {
-	const int size = 5;
-	
-	int array[size][size];
-	int quantity = 0; 
-	
+void print(int (&a)[10][10], int size) {
 	for (int i = 0; i < size; i++) {
 		for (int j = 0; j < size; j++) {
-			array[i][j] = random(2);
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+int main() {
+	const int size = 10;
+	
+	int array[size][size] = {0};
+	int quantity = 0; 
+	
+	array[size/2][size/2] = 1;	    
+	quantity++;
+	/*
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			array[i][j] = random(1);
 			if (array[i][j] == 1) {
 				quantity++;
 			}
 		}
-	}	    
-	
+	}
+	*/
+	print(array, size);
 	int count = 0;
 	while (quantity > 0) {
 		step(array, size, quantity);
 		count++;
-		cout << quantity << endl;
+		//cout << quantity << endl;
 	}
+	
+	cout << count << endl;
+	print(array, size);
 	return 0;
 }
