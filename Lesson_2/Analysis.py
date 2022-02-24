080ba055a246ecdf220afb1244d2b81759ad7fcf
Klimanov <klimanov.dd@phystech.edu>
2022-02-24 17:12:48 +0300

    Only graphs and widening of values remains.

--- Lesson_2/Analysis.py
+++ Lesson_2/Analysis.py
@@ -7,10 +7,15 @@ Let disposition be located in the centre of the cristall.
 I suppose that cristall has not even N (in order to make
 all directions equal to each other).
 For each N there was completed 10 experiments. 
-"""
-N1 = [5, 9, 13, 17, 21]
+
+N1 = [5, 9, 13, 17, 21, 25]
 Steps1 = [4.5, 18.5, 42, 74, 117, 169]
 
+plt.title('Steps as function of Area')
+plt.plot(N1, Steps1)
+plt.show()
+"""
+"""
 mu, sigma = 100, 15
 x = mu + sigma * np.random.randn(10000)
 # histogram for the second experiment
@@ -22,3 +27,14 @@ plt.title('Steps(Area)')
 plt.axis([50, 160, 0, 0.04])
 plt.grid(True)
 plt.show()
+"""
+"""
+2)
+"""
+X = [0.45, 0.46, 0.47, 0.48, 0.49, 0.50, 0.51, 0.52, 0.53, 0.54]
+Y = [6.36, 5.69, 5.22, 4.87, 4.63, 4.39, 4.18, 4.03, 3.46, 3]
+
+plt.title('Steps as function of Area')
+plt.plot(X, Y)
+plt.show()
+
--- Lesson_2/Experiment_2/main.cpp
+++ Lesson_2/Experiment_2/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <random>
 #include <chrono>
+#include <cmath>
 
 using namespace std;
 
 mt19937 rng(chrono::system_clock::now().time_since_epoch().count());
 uniform_int_distribution<int> dist(0, 3);
+
+double eps = 0.00001f;
 
 bool check(int (&array)[40][40], int i, int j) {
 	bool flag = false;
@@ -90,30 +93,84 @@ void print(int (&a)[40][40], int size) {
 	}
 }
 
+void recalculate(int count, double (&result)[10], int (&num_exp)[10], double Area) {
+	if ((Area - 0.46) < 0) {
+		result[0] = (result[0]*num_exp[0] + count)/(num_exp[0] + 1);
+		num_exp[0]++;
+	} else if ((Area - 0.47) < 0) {
+		result[1] = (result[1]*num_exp[1] + count)/(num_exp[1] + 1);
+		num_exp[1]++;
+	} else if ((Area - 0.48) < 0) {
+		result[2] = (result[2]*num_exp[2] + count)/(num_exp[2] + 1);
+		num_exp[2]++;
+	} else if ((Area - 0.49) < 0) {
+		result[2] = (result[2]*num_exp[2] + count)/(num_exp[2] + 1);
+		num_exp[2]++;
+	} else if ((Area - 0.50) < 0) {
+		result[3] = (result[3]*num_exp[3] + count)/(num_exp[3] + 1);
+		num_exp[3]++;	
+	} else if ((Area - 0.51) < 0) {
+		result[4] = (result[4]*num_exp[4] + count)/(num_exp[4] + 1);
+		num_exp[4]++;
+	} else if ((Area - 0.52) < 0) {
+		result[5] = (result[5]*num_exp[5] + count)/(num_exp[5] + 1);
+		num_exp[5]++;
+	} else if ((Area - 0.53) < 0) {
+		result[6] = (result[6]*num_exp[6] + count)/(num_exp[6] + 1);
+		num_exp[6]++;
+	} else if ((Area - 0.54) < 0) {
+		result[7] = (result[7]*num_exp[7] + count)/(num_exp[7] + 1);
+		num_exp[7]++;
+	} else if ((Area - 0.55) < 0) {
+		result[8] = (result[8]*num_exp[8] + count)/(num_exp[8] + 1);
+		num_exp[8]++;
+	} else {
+		result[9] = (result[9]*num_exp[9] + count)/(num_exp[9] + 1);
+		num_exp[9]++;
+	}
+}
+
 int main() {
 	const int size = 40;
+	int array[size][size];
+	int quantity;
+	double Area;
 	
-	int array[size][size] = {0};
-	int quantity = 0; 
+	double result[10] = {0};
+	int num_exp[10] = {0};
+	//"result" is an array of const size, related to values of Area
+	// 0.45, 0.46, 0.47, 0.48, 0.49, 0.50, 0.51, 0.52, 0.53
+	// "num_exp" - array, that states number of reculculating of each
+	// position in "result" array
 	
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			array[i][j] = dist(rng)%2;
-			if (array[i][j] == 1) {
-				quantity++;
+	for (int k = 0; k < 20000; k++) {
+		array[size][size] = {0};
+		quantity = 0; 
+		
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				array[i][j] = dist(rng)%2;
+				if (array[i][j] == 1) {
+					quantity++;
+				}
 			}
 		}
-	}
-	
-	double Area = (double)quantity/(size*size);
 		
-	int count = 0;
-	while (quantity > 0) {
-		step(array, size, quantity);
-		count++;
+		Area = (double)quantity/(size*size);
+			
+		int count = 0;
+		while (quantity > 0) {
+			step(array, size, quantity);
+			count++;
+		}
+		--count;
+		
+		recalculate(count, result, num_exp, Area);
 	}
-	
-	cout << --count << " " << Area << endl;
-	
+	cout << endl;
+	for (int k = 0; k < 10; k++) {
+		cout << result[k] << " " << num_exp[k] << endl;
+	}
+
 	return 0;
 }
